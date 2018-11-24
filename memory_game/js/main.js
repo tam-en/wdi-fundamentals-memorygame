a2e87815c0d90c91b67d84ba21db9a32097e69f0
Tamis Nordling <rushling@Tams-2017-MBP.hsd1.wa.comcast.net>
2018-11-24 12:37:03 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,21 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-// cardOne represents the first card the user flips
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardOne + ' durn it.');
-
-// cardTwo represents the second card the user flips
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardTwo + ' durn it again.');
-
-//check whether two cards have been flipped, and if so, report match status.
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	} else {
-		alert('Sorry, try again.');
-	}
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!');
+		} else {
+			alert('Sorry, try again.');
+		}
+	};
 };
 
+var flipCard = function(cardId) {
+    console.log('User flipped ' + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
